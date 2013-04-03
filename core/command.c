890afd87dd014c03ef13c1dc2c1f74c8cb54c04f
wm4 <wm4@nowhere>
2013-04-04 00:28:28 +0200

    command: remove bogus vsync property

--- core/command.c
+++ core/command.c
@@ -1088,13 +1088,6 @@ static int mp_property_gamma(m_option_t *prop, int action, void *arg,
     return M_PROPERTY_UNAVAILABLE;
 }
 
-/// VSync (RW)
-static int mp_property_vsync(m_option_t *prop, int action, void *arg,
-                             MPContext *mpctx)
-{
-    return mp_property_generic_option(prop, action, arg, mpctx);
-}
-
 /// Video codec tag (RO)
 static int mp_property_video_format(m_option_t *prop, int action,
                                     void *arg, MPContext *mpctx)
@@ -1419,7 +1412,6 @@ static const m_option_t mp_properties[] = {
     M_OPTION_PROPERTY_CUSTOM_("hue", mp_property_gamma,
                     .offset = offsetof(struct MPOpts, gamma_hue)),
     M_OPTION_PROPERTY_CUSTOM("panscan", mp_property_panscan),
-    M_OPTION_PROPERTY_CUSTOM_("vsync", mp_property_vsync),
     { "video-format", mp_property_video_format, CONF_TYPE_STRING,
       0, 0, 0, NULL },
     { "video-codec", mp_property_video_codec, CONF_TYPE_STRING,
